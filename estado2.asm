ddd1eceb1dbfafcbca1282ae81fa9a2b0151db97
Guilherme Cillo <guilhermecillo2016@gmail.com>
2026-06-21 21:35:33 -0300

    Estado 2 : Armado parcialmente feito

--- estado2.asm
+++ estado2.asm
@@ -1,11 +1,26 @@
+.data 
+	tamanho_senha: .word 4
+	
 .text
 .globl imprime2
 imprime2:
-
-    li $a0, 2
-    li $v0, 1
-    syscall
-
-    jal imprime1
-
-    jr $ra
+	# Escreve L no display esquerdo 
+    	lw	$t0, tamanho_senha
+    	li	$t1, 0
+#Ler o teclado
+leitura:
+	li	$v0, 5
+	syscall
+	#Verificar as trancas
+	bge $v0, 10, dispara_alarme
+	addi	$t1, $t1, 1
+	addi	$sp, $sp, -4
+	move	$sp, $v0
+	blt   	$t1, $t0, leitura 
+	# verificar a senha
+	addi	$sp, $sp, -4
+    move	$sp, $ra
+	jal verificar_a_senha 
+	
+dispara_alarme: 
+	jal estado_3
