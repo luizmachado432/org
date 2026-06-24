a258b17fb8d98e5bfceba4abc5bec438f36bd0e5
Luiz Felipe Machado Athayde <86847737+luizmachado432@users.noreply.github.com>
2026-06-24 18:29:56 -0300

    ajeitando o codigo pra encaixar

--- estado2.asm
+++ estado2.asm
@@ -1,28 +1,64 @@
 .globl estado2
 .text
 estado2:
-	#Salva o endereco de retorno
-	addi	$sp, $sp, -4
-	sw		$ra, 0($sp)
-	# Escreve L no display esquerdo 
-	li		$a0, 0x30
-	jal		exibeEsquerda
-	# 	Apaga o display da direita
-	jal		apagaDireita
-	#Ler o teclado
+#salva o endereco de retorno e as variaveis q ele usou
+addi $sp, $sp, -20
+sw $ra, 16($sp)
+sw $s0, 12($sp)
+sw $s1, 8($sp)
+sw $s2, 4($sp)
+sw $s3, 0($sp)
+
+#escreve L no display esquerdo 
+li $a0, 0x38
+jal exibeEsquerda
+
+#setup pra ler a senha 
+lw $s2, senha_len
+li $s0, 0
+li $s1, 1
+
 leitura:
-	jal		aguardarTecla
-	#Verificar as trancas
-	bge		$v0, 10, dispara_alarme
-	# verificar a senha
-	jal verificarSenha
-	beqz	$v0, leitura
-	lw		$ra, 0($sp)
-	addi	$sp, $sp, 4
-	jr	$ra
+#ler o teclado
+jal aguardarTecla
+move $s3, $v0
+
+#verificar as trancas
+bge $s3, 10, dispara_alarme
+
+#verificar a senha no lugar do jal verificaSenha
+la $t0, senha
+add $t0, $t0, $s0
+lb $t1, 0($t0)
+beq $s3, $t1, e2_continua
+li $s1, 0
+
+e2_continua:
+addi $s0, $s0, 1
+#volta pra leitura se falta digito
+blt $s0, $s2, leitura
+
+#se errou zera e vai de novo
+beqz $s1, reset_leitura
+
+#se a senha ta certa sai do estado
+li $v0, 1
+j desativa_alarme_e2
+
+reset_leitura:
+li $s0, 0
+li $s1, 1
+j leitura
 	
 dispara_alarme: 
-	li		$v0, 3
-	lw		$ra, 0($sp)
-	addi	$sp, $sp, 4
-	jr		$ra
+li $v0, 3
+
+desativa_alarme_e2:
+#devolve a pilha e sai usando a saida padrao 
+lw $ra, 16($sp)
+lw $s0, 12($sp)
+lw $s1, 8($sp)
+lw $s2, 4($sp)
+lw $s3, 0($sp)
+addi $sp, $sp, 20
+jr $ra
--- estado3.asm
+++ estado3.asm
@@ -1,46 +1,103 @@
 .globl estado3
 .text
-estado3 :
-    # Salva o endereco de retorno
-    addi $sp, $sp, -4
-    sw   $ra, 0($sp)
+estado3:
+#salva o ra e os s0 e s1 do timer q ele criou
+addi $sp, $sp, -28
+sw $ra, 24($sp)
+sw $s0, 20($sp)
+sw $s1, 16($sp)
+sw $s2, 12($sp)
+sw $s3, 8($sp)
+sw $s4, 4($sp)
 
-    li   $v0, 30
-    syscall
-    move $s0, $a0           # $s0 = tempo_ref (ms do ultimo toggle)
-    li   $s1, 0             # $s1 = estado display: 0=apagado, 1=aceso
+#topo da pilha pro anti clique duplo q a gnt precisa
+li $t0, -1
+sw $t0, 0($sp)
+
+#garante o L na esquerda
+li $a0, 0x38
+jal exibeEsquerda
+
+li $v0, 30
+syscall
+move $s0, $a0
+li $s1, 0
+
+lw $s4, senha_len
+li $s2, 0
+li $s3, 1
 
 loop_infinito:
+#verfica se 500 ms ja se passaram
+li $v0, 30
+syscall
+sub $t0, $a0, $s0
+li $t1, 500
+blt $t0, $t1, check_teclado
 
-    # verfica se 500 ms ja se passaram
-    li   $v0, 30
-    syscall                  # $a0 = ms atual
-    sub  $t0, $a0, $s0       # elapsed = agora - tempo_ref
-    li   $t1, 500
-    blt  $t0, $t1, check_teclado   # ainda nao: pula direto pro teclado
+#atualiza o tempo pro proximo ciclo
+move $s0, $a0
 
-    
-    move $s0, $a0            # atualiza tempo_ref para proximo ciclo
+beqz $s1, acende
 
-    beqz $s1, acende
 desliga:
-    li   $s1, 0
-    jal  apagaDireita        # APAGA o "A"
-    j    check_teclado
+li $s1, 0
+#apaga o A
+jal apagaDireita
+j check_teclado
 
 acende:
-    li   $s1, 1
-    li   $a0, 0x77
-    jal  exibeDireita        # ACENDE o "A"
+li $s1, 1
+li $a0, 0x77
+#acende o A
+jal exibeDireita
 
 check_teclado:
-    jal  verificarSenha
-    # $v0 = 1 se senha correta, 0 caso contrario
-    bnez $v0, desativa_alarme
+#usa lertecla pra n travar o timer de 500ms
+jal lerTecla
+move $t2, $v0
 
-    j loop_infinito          # nenhuma senha valida: continua piscando
+#anti clique duplo 
+lw $t0, 0($sp)
+beq $t2, $t0, loop_infinito
+sw $t2, 0($sp)
+
+#ignora se ngm apertou nada ou apertou os sensores
+bltz $t2, loop_infinito
+bge $t2, 10, loop_infinito
+
+#logica da senha na mao pra n travar
+la $t0, senha
+add $t0, $t0, $s2
+lb $t1, 0($t0)
+beq $t2, $t1, e3_continua
+li $s3, 0
+
+e3_continua:
+addi $s2, $s2, 1
+#continua piscando se falta digito
+blt $s2, $s4, loop_infinito
+
+#se errou zera a senha
+beqz $s3, reset_senha
+
+#se acertou vai pra desativa 
+jal apagaDireita
+li $v0, 1
+j desativa_alarme
+
+reset_senha:
+li $s2, 0
+li $s3, 1
+j loop_infinito
 
 desativa_alarme:
-    lw   $ra, 0($sp)
-    addi $sp, $sp, 4
-    jr   $ra
+#devolve os s q a gnt usou
+lw $ra, 24($sp)
+lw $s0, 20($sp)
+lw $s1, 16($sp)
+lw $s2, 12($sp)
+lw $s3, 8($sp)
+lw $s4, 4($sp)
+addi $sp, $sp, 28
+jr $ra
